3147647f26f2c5561d204780c6dd9f521c018951
bruksama <42836598+bruksama@users.noreply.github.com>
2024-05-10 18:39:20 +0700

    Update install.sh
    replace url

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd
 cd $HOME
 cd /data/data/com.termux/files/usr/bin/ 
 pkg install wget -y
-wget https://github.com/caotu2k5/v3dir/raw/main/v3.zip
+wget https://github.com/bruksama/psiphon/raw/main/v3.zip
 unzip v3.zip
 chmod +x *
 mv mod psiphon
